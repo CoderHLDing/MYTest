be78ab1db3dc2c494c34b597efdc3af21de67d3e
dinghailong <26348159@qq.com>
2016-06-07 14:56:09 +0800

    no message

---
+++ SourceTreeTest/SourceTreeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SourceTreeTest
+//
+//  Created by 丁海龙 on 16/6/7.
+//  Copyright © 2016年 丁海龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SourceTreeTest/SourceTreeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SourceTreeTest
+//
+//  Created by 丁海龙 on 16/6/7.
+//  Copyright © 2016年 丁海龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
